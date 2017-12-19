797657d53d234067dcfb2706980fe6c37a3be80d
Siddharth <siddharth@nisostech.com>
2017-12-19 22:40:44 +0530

    page redirection issue

--- public/form_modules/forms/base/directives/submit-form.client.directive.js
+++ public/form_modules/forms/base/directives/submit-form.client.directive.js
@@ -786,12 +786,12 @@ angular.module('view-form').directive('submitFormDirective', ['$http', 'TimeCoun
                            window.location.href = window.location.origin + "/" + formAction + "?id=" + $rootScope.patientId;
 						}
 						else{
-							window.location = $scope.currentPageUrl;
+							// window.location = $scope.currentPageUrl;
 						}
 						
 					}, function (reason) {
 						console.log("reason : ", reason);
-						window.location = $scope.currentPageUrl;
+						// window.location = $scope.currentPageUrl;
 						return false;
 					});
 
