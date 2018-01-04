7e19733cd3cde422d6222aac9fb75a53a2f7818f
Siddharth <siddharth@nisostech.com>
2018-01-04 13:21:32 +0530

    add as-date.js directive

---
+++ public/form_modules/forms/base/directives/as-date.js
@@ -0,0 +1,17 @@
+app.directive('asDate', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, modelCtrl) {
+            modelCtrl.$formatters.push(function (input) {
+                var transformedInput;
+                if (input) transformedInput = new Date(input);
+                else transformedInput = new Date();
+                if (transformedInput !== input) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
+                }
+                return transformedInput;
+            });
+        }
+    };
+});
